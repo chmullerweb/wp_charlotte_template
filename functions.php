f85fbac4a0184a525925008977ecabd063b50d75
chmullerweb <ch.muller.web@gmail.com>
2020-07-07 16:50:44 +0200

    Configuration d'une searchbar

--- functions.php
+++ functions.php
@@ -1,16 +1,27 @@
-<?php 
+<?php
 //------------------------------------------------------------------------------------------
-  // Inclure les styles et script       
+// Inclure les styles et script       
 //------------------------------------------------------------------------------------------
 
 function montheme_register_assets()
 {
-      //affiche le style défini avant celui de WP par défaut
-    wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital@0;1&family=Oswald:wght@500;600;700&display=swap');
-    wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/style.css', false,'1.1','all');
-    wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
-    wp_enqueue_script( 'fontawesome', 'https://kit.fontawesome.com/39ca2ad730.js', [], false, true);
-    wp_enqueue_script('javascript', 'assets/js/main.js', [], false, true );
+    //intégration du framework Bootstrap
+    wp_register_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css');
+    wp_register_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', [], false, true);
+    //WP utilise un script jquery par défaut. Si je veux le personnaliser, il faut d'abord que "j'efface" la version WP. Attention ! Car certains plugins utilise le jquery WP 
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.slim.min.js', [], false, true);
+
+
+    //affiche le style défini avant celui de WP par défaut
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+    wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:ital@0;1&family=Oswald:wght@500;600;700&display=swap');
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css', false, '1.1', 'all');
+    wp_enqueue_script('fontawesome', 'https://kit.fontawesome.com/39ca2ad730.js', [], false, true);
+    wp_enqueue_script('javascript', 'assets/js/main.js', [], false, true);
 }
 
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
@@ -21,15 +32,16 @@ add_action('wp_enqueue_scripts', 'montheme_register_assets');
 # Menu Footer
 # Logo
 //------------------------------------------------------------------------------------------
-function montheme_setup(){
+function montheme_setup()
+{
     //mon thème "supporte" le logo customisé
-    add_theme_support( 'custom-logo' );
+    add_theme_support('custom-logo');
 
     //mon thème "supporte" les menus
     add_theme_support('menus');
     //je configure le menu header et footer en lui donnant un nom et une description
-    register_nav_menu( 'nav_header', 'En tête du menu' );
-    register_nav_menu( 'nav_footer', 'Menu en pied de page' );
+    register_nav_menu('nav_header', 'En tête du menu');
+    register_nav_menu('nav_footer', 'Menu en pied de page');
 }
 
 add_action('after_setup_theme', 'montheme_setup');
@@ -37,85 +49,87 @@ add_action('after_setup_theme', 'montheme_setup');
 //------------------------------------------------------------------------------------------
 // Définir la zone de widgets
 //------------------------------------------------------------------------------------------
-function montheme_init_widgets() {
+function montheme_init_widgets()
+{
     # Zone de widget 1
-      register_sidebar(
-          array(
-              'id' => 'widgets-section-1', //ID de la div 
-              'name' => 'Zone de widgets 1', //nom du widget 
-             'description' => 'Description de la zone de widgets',
-             'before_widget' => '<div class="widget">', //ouvrir la balise <div> pour englober le widget 
-             'after_widget' => '</div>',  //fermer la balise 
-             'before_title' => '<h4 class="widget-title">', //ouvrir <h4> pour le titre du widget
-             'after_title' => '</h4>' //fermer la balise titre
-             )
-          );
+    register_sidebar(
+        array(
+            'id' => 'widgets-section-1', //ID de la div 
+            'name' => 'Zone de widgets 1', //nom du widget 
+            'description' => 'Description de la zone de widgets',
+            'before_widget' => '<div class="widget">', //ouvrir la balise <div> pour englober le widget 
+            'after_widget' => '</div>',  //fermer la balise 
+            'before_title' => '<h4 class="widget-title">', //ouvrir <h4> pour le titre du widget
+            'after_title' => '</h4>' //fermer la balise titre
+        )
+    );
     // Zones de widgets 2 et 3
     register_sidebar(
         array(
-            'id' => 'widgets-section-2', 
-            'name' => 'Zone de widgets 2', 
-           'description' => 'Description de la zone de widgets',
-           'before_widget' => '<div class="widget">', 
-           'after_widget' => '</div>',  
-           'before_title' => '<h4 class="widget-title">', 
-           'after_title' => '</h4>' 
-           )
-        );
+            'id' => 'widgets-section-2',
+            'name' => 'Zone de widgets 2',
+            'description' => 'Description de la zone de widgets',
+            'before_widget' => '<div class="widget">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
     register_sidebar(
         array(
-            'id' => 'widgets-section-3', 
-            'name' => 'Zone de widgets 3', 
+            'id' => 'widgets-section-3',
+            'name' => 'Zone de widgets 3',
             'description' => 'Description de la zone de widgets',
-            'before_widget' => '<div class="widget">', 
-            'after_widget' => '</div>',  
-            'before_title' => '<h4 class="widget-title ">', 
-            'after_title' => '</h4>' 
-            )
-        );
+            'before_widget' => '<div class="widget">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class="widget-title ">',
+            'after_title' => '</h4>'
+        )
+    );
     // Zone de widgets 4
     register_sidebar(
         array(
-            'id' => 'widgets-section-4', 
-            'name' => 'Zone de widgets 4', 
+            'id' => 'widgets-section-4',
+            'name' => 'Zone de widgets 4',
             'description' => 'Description de la zone de widgets',
-            'before_widget' => '<div class="widget">', 
-            'after_widget' => '</div>',  
-            'before_title' => '<h4 class="widget-title">', 
-            'after_title' => '</h4>' 
-            )
-        );
+            'before_widget' => '<div class="widget">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
     // Zone de widgets sidebar
     register_sidebar(
         array(
-            'id' => 'sidebar', 
-            'name' => 'Zone de widgets Sidebar', 
+            'id' => 'sidebar',
+            'name' => 'Zone de widgets Sidebar',
             'description' => 'Description de la zone de widgets',
-            'before_widget' => '<div class="widget">', 
-            'after_widget' => '</div>',  
+            'before_widget' => '<div class="widget">',
+            'after_widget' => '</div>',
             'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>' 
-            )
-        );
+            'after_title' => '</h3>'
+        )
+    );
 }
 
 // Afficher la zone de Widgets
 add_action('widgets_init', 'montheme_init_widgets');
 
 // Afficher le contenu du widget choisi - sinon si la zone widget est vide, créer un aperçu de la zone. Cette fonction est appelée dans footer.php
-function montheme_display_widget($widgetId){
+function montheme_display_widget($widgetId)
+{
     //vérifiez si la zone de widget contient du contenu avec en paramètre l'ID de la zone
-    if(is_active_sidebar($widgetId)) {
+    if (is_active_sidebar($widgetId)) {
         //la fonction range/affiche le widget dans la zone widget renseignée par son ID
         dynamic_sidebar($widgetId);
-      } else {
-          ?>
-          <div class="widget">
-          <h4 class="widget-title">Aperçu zone de widget</h4>
-          <p>Croissant jelly chocolate cake fruitcake lollipop cake. Croissant brownie jelly beans cupcake. Donut gummies gummi bears marzipan</p>
-          </div>
+    } else {
+?>
+        <div class="widget">
+            <h4 class="widget-title">Aperçu zone de widget</h4>
+            <p>Croissant jelly chocolate cake fruitcake lollipop cake. Croissant brownie jelly beans cupcake. Donut gummies gummi bears marzipan</p>
+        </div>
 <?php
-}
+    }
 }
 
 
--- header.php
+++ header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <!-- Je récupère les infos sur le charset, la description, l'auteur et le nom avec bloginfo -->
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="description" content="<?php bloginfo('description'); ?>">
-    <meta name="author" content="<?php bloginfo('author');?>">
+    <meta name="author" content="<?php bloginfo('author'); ?>">
     <title><?php bloginfo('name'); ?></title>
 
     <!-- J'appelle la fonction wp_head pour inclure des paramètres d'en-tête WP -->
@@ -12,67 +13,68 @@
 </head>
 
 <!-- J'ajoute la class body_class afin d'afficher la barre de personnalisation -->
+
 <body <?php body_class(); ?>>
 
-<header class="site-header">
-    <div class="site-branding">
-        <!-- J'affiche un logo s'il existe, sinon j'affiche le nom du site via un <a>. -->
-        <?php
-        if(has_custom_logo()){
-            the_custom_logo();
-        } else {
-        ?>
-        <!-- Je récupère l'url de l'index.php et le nom du site avec bloginfo -->
-      <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
-      <?php } ?>
-    </div>
+    <header class="site-header">
+        <div class="site-branding">
+            <!-- J'affiche un logo s'il existe, sinon j'affiche le nom du site via un <a>. -->
+            <?php
+            if (has_custom_logo()) {
+                the_custom_logo();
+            } else {
+            ?>
+                <!-- Je récupère l'url de l'index.php et le nom du site avec bloginfo -->
+                <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+            <?php } ?>
+        </div>
 
-    <!-- Je crée une navigation header dynamique avec wp_nav_menu -->
-    <nav class="navigation navigation-top desktop-navigation">
+        <!-- Je crée une navigation header dynamique avec wp_nav_menu -->
+        <nav class="navigation navigation-top desktop-navigation">
 
-    <?php 
-    //La zone de menu et son contenu à récupérer (toujours nommer la clé 'theme_location')
-    wp_nav_menu(
-        array(
-        'theme_location' => 'nav_header',
-        'container' => false // évite que WP engloabe le <ul> dans une balise <div>
-        )
-    );
-    ?>
-    </nav>
-    <nav class="navigation navigation-top mobile-navigation">
-      <ul>
-        <li class="mobile-menu"><i class="fa fa-bars fa-1x"></i>
-        <?php
-          wp_nav_menu(
-            array(
-              'theme_location'  => 'nav_header',
-              'menu_class'      => 'sub-navigation', // liste des classes CSS que WP doit ajouter à la balise <ul> créée
-              'container'       => false
-            )
-          );
-           ?>
-        </li>
-      </ul>
-    </nav>
-    <nav class="navigation navigation-aside">
-      <ul>
-        <li class="search-toggle">
-          <a href="#"><i class="fas fa-search"></i>
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <i class="fas fa-shopping-cart"></i>
-            <span class="count cart-counter">0</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <section class="search-popup">
-      <form class="search-form" action="index.html" method="post">
-        <input type="text" name="search" value="" placeholder="Tapez quelque chose...">
-        <input type="submit" name="submit" value="Rechercher">
-      </form>
-    </section>
-  </header>
+            <?php
+            //La zone de menu et son contenu à récupérer (toujours nommer la clé 'theme_location')
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'nav_header',
+                    'container' => false // évite que WP engloabe le <ul> dans une balise <div>
+                )
+            );
+            ?>
+        </nav>
+        <nav class="navigation navigation-top mobile-navigation">
+            <ul>
+                <li class="mobile-menu"><i class="fa fa-bars fa-1x"></i>
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location'  => 'nav_header',
+                            'menu_class'      => 'sub-navigation', // liste des classes CSS que WP doit ajouter à la balise <ul> créée
+                            'container'       => false
+                        )
+                    );
+                    ?>
+                </li>
+            </ul>
+        </nav>
+        <nav class="navigation navigation-aside">
+            <ul>
+                <li class="search-toggle">
+                    <!-- Insérer une barre de recherche selon le template searchform.php -->
+                    <?php echo get_search_form(); ?>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fas fa-shopping-cart"></i>
+                        <span class="count cart-counter">0</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <section class="search-popup">
+            <form class="search-form" action="index.html" method="post">
+                <input type="text" name="search" value="" placeholder="Tapez quelque chose...">
+                <input type="submit" name="submit" value="Rechercher">
+            </form>
+        </section>
+    </header>
