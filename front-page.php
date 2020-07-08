cfa882978134b925d45696292622cb78f5c196a4
chmullerweb <ch.muller.web@gmail.com>
2020-07-08 18:15:13 +0200

    Configuration de la front-page.php

---
+++ front-page.php
@@ -0,0 +1,77 @@
+<?php get_header(); ?>
+
+  <section class="page-header front-page-header" style="background-image:url(<?php echo get_theme_mod('banner_image');?>)">
+    <h1 class="page-title"><?php echo get_theme_mod('homepage_cover_title');?></h1>
+    <a href="#">
+      <button type="button" class="call-to-action">
+        Réserver mon billet
+      </button>
+  </a>
+  </section>
+  <main class="container front-content">
+    <section class="custom-presentation">
+      <img src="<?php echo get_theme_mod('presentation_image'); ?>">
+      
+      <div class="custom-presentation-infos">
+        <h2 class="custom-title"><?php echo get_theme_mod('presentation_title_text');?></h2>
+        <p class="custom-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing,
+          sed do eiusmod tempor incididunt.
+
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+          quae ab illo inventore veritatis et quasi architecto beatae vitae
+          dicta sunt explicabo.
+
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat
+          nulla pariatur.
+        </p>
+        <a href="page.html">
+          <button type="button" class="call-to-action">En savoir plus</button>
+        </a>
+      </div>
+    </section>
+    <section class="custom-blocks">
+      <div class="custom-block">
+        <i class="fas fa-birthday-cake fa-3x"></i>
+        <h3 class="custom-title">Concert célébration</h3>
+        <p class="custom-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing,
+          sed do eiusmod tempor incididunt.
+        </p>
+      </div>
+      <div class="custom-block">
+        <i class="fas fa-music fa-3x"></i>
+        <h3 class="custom-title">L'amour de la musique</h3>
+        <p class="custom-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing,
+          sed do eiusmod tempor incididunt.
+        </p>
+      </div>
+      <div class="custom-block">
+        <i class="fas fa-ticket-alt fa-3x"></i>
+        <h3 class="custom-title">+3000 tickets en vente</h3>
+        <p class="custom-content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing,
+          sed do eiusmod tempor incididunt.
+        </p>
+      </div>
+    </section>
+    <section class="custom-section">
+      <blockquote class="star-quote">
+        <p class="quote-content">
+          Ce qui se conçoit bien s'énonce clairement et les mots pour le dire
+          arrivent aisément.
+        </p>
+        <cite class="quote-footer">
+          Boileau
+        </cite>
+      </blockquote>
+    </section>
+  </main>
+ 
+  <?php get_footer(); ?>
--- inc/customizer.php
+++ inc/customizer.php
@@ -79,6 +79,175 @@ function montheme_customize_register($wp_customize){
         )
     );
 
+    //------------------------------------------------------------------------------------------
+            //Créer une checkbox pour afficher le widget latéral
+    //------------------------------------------------------------------------------------------
+
+//     // TODO - En autonomie - Ajouter un nouveau paramètre au Customizer
+//   // identifiant : 'fp_container_boxy'
+//   // valeur par défaut : false (booléen)
+//   // type : 'theme_mod'
+//   $wp_customize -> add_setting(
+//     'sidebar_boxy',
+//       array(
+//           'default' => 'Oui',
+//           'type' => 'theme_mod' 
+//           )
+//   );
+
+
+// // TODO - En autonomie - Ajouter un contrôleur associé au paramètre 'fp_container_boxy'
+// // libellé : 'Affichage avec effet boxy'
+// // description : 'Style du conteneur de la page d\'accueil.'
+// // section : 'fp_container'
+// // type : case à cocher - Voir doc pour connaître la valeur à passer :
+// // https://developer.wordpress.org/reference/classes/wp_customize_control/__construct/
+// $wp_customize -> add_control(
+//       'sidebar_boxy',
+//       array(
+//           'label' => 'Afficher le widget',
+//           'description' => 'Est-ce que le widget latéral est visible ?',
+//           'section' => 'sidebar', //section dans laquelle s'afficher 
+//           'type' => 'checkbox'
+//       )
+// );
+
+    //------------------------------------------------------------------------------------------
+            //Enregistre un nouveau dossier 'Page d'Accueil' au customizer
+    //------------------------------------------------------------------------------------------
+
+
+    $wp_customize -> add_panel(
+        'homepage_panel', 
+        array(
+            'title' => 'Editer la page d\'acceuil',
+        )
+    );
+
+    //La section pour l'image en couverture 
+    $wp_customize->add_section(
+        'homepage_cover_image',
+        array(
+            'title' => 'Images de Couverture', 
+            'description' => 'Editer la photo en couverture de la page d\'acceuil', 
+            'panel' => 'homepage_panel' 
+        )
+    );
+
+    $wp_customize -> add_setting(
+        'banner_image',
+        array(
+            'default' => get_template_directory() . '/assets/images/alex-bracken-unsplash.jpg',
+            'type' => 'theme_mod' 
+        )
+    );
+
+    $wp_customize -> add_control(
+        new WP_Customize_Image_control(
+            $wp_customize, 
+            'banner_image',
+            array(
+                'label' => 'Image en couverture',
+                'section' => 'homepage_cover_image', 
+                'settings' => 'banner_image'  
+            )
+
+        )
+    );
+
+    //La section pour le texte en couverture
+    $wp_customize->add_section(
+        'homepage_cover_text',
+        array(
+            'title' => 'Texte en couverture', 
+            'description' => 'Editer le texte en couverture de la page d\'acceuil', 
+            'panel' => 'homepage_panel' 
+        )
+    );
+
+    $wp_customize -> add_setting(
+        'homepage_cover_title',
+        array(
+            'default' => 'Bientôt en concert',
+            'type' => 'theme_mod' 
+        )
+    );
+
+    $wp_customize -> add_control(
+            'homepage_cover_title',
+            array(
+                'label' => 'Titre principal',
+                'description' => 'Ecrire le titre principal en couverture',
+                'section' => 'homepage_cover_text', 
+                'settings' => 'homepage_cover_title',
+                'type' => 'text'  
+            )
+
+            );
+
+    //La section pour l'image en présentation
+    $wp_customize->add_section(
+        'homepage_presentation_image',
+        array(
+            'title' => 'Images de Présentation', 
+            'description' => 'Editer la photo en présentation de la page d\'acceuil', 
+            'panel' => 'homepage_panel' 
+        )
+    );
+
+    $wp_customize -> add_setting(
+        'presentation_image',
+        array(
+            'default' => get_template_directory() . '/assets/images/yannis-papanastasopoulos-unsplash.jpg',
+            'type' => 'theme_mod' 
+        )
+    );
+
+    $wp_customize -> add_control(
+        new WP_Customize_Image_control(
+            $wp_customize, 
+            'presentation_image',
+            array(
+                'label' => 'Image de présentation',
+                'section' => 'homepage_presentation_image', 
+                'settings' => 'presentation_image'  
+            )
+
+        )
+    );
+
+    //La section pour le texte de présentation
+    $wp_customize->add_section(
+        'homepage_presentation_text',
+        array(
+            'title' => 'Texte de Présentation', 
+            'description' => 'Editer le texte de présentation de la page d\'acceuil', 
+            'panel' => 'homepage_panel' 
+        )
+    );
+
+    $wp_customize -> add_setting(
+        'presentation_title_text',
+        array(
+            'default' => 'Un super groupe de musique',
+            'type' => 'theme_mod' 
+        )
+    );
+
+    $wp_customize -> add_control(
+            'presentation_title_text',
+            array(
+                'label' => 'Titre de présentation',
+                'description' => 'Ecrire le texte de présentation',
+                'section' => 'homepage_presentation_text', 
+                'settings' => 'presentation_title_text',
+                'type' => 'text'  
+            )
+
+            );
+    
+
 };
 
 add_action( 'customize_register', 'montheme_customize_register' );
+
