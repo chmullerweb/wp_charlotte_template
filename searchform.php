60ccf50e327929483fc639b3c0cd6437628d3698
chmullerweb <ch.muller.web@gmail.com>
2020-07-12 15:12:36 +0200

    Redirection 404 si search vide

--- searchform.php
+++ searchform.php
@@ -1,4 +1,12 @@
-<form class="search-form" action="<?php echo esc_url(home_url('/')); ?>" method="post">
+<form class="search-form" action="
+<?php
+ if (empty($_GET['s']) || $_GET['s'] === '') {
+     echo esc_url(home_url('/404'));
+ } else {
+    echo esc_url(home_url('/'));
+}
+?>"
+method="get">
                 <input type="search" aria-label="Search" name="s" value="<?php echo get_search_query( );?>" placeholder="Tapez quelque chose...">
                 <input type="submit" name="submit" value="Rechercher">
 </form>
